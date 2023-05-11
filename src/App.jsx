617c42b2f75c980ecd49116bda28a7e06893a8b4
unknown <amirzanganeh1999@gmail.com>
2023-05-11 16:44:37 +0330

    Design Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import DesktopMainPage from './components/DesktopMainPage'
+import Header from './components/Header'
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <Header/>
+      <DesktopMainPage/>
+    </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Person,Search,Cart3 ,ChevronDown} from 'react-bootstrap-icons';
+import Logo from "../assets/Logo.png"
+import styles from "../Styles/Header.module.scss"
+export default function Header() {
+  return (
+    <header className={styles.header}>
+    <div>
+        <img src={Logo} alt="logo" />
+    </div>
+    <div>
+        <ul className={styles.menu}>
+            <li>صفحه اصلی</li>
+            <li>شعبه<ChevronDown/></li>
+            <li>منو<ChevronDown/></li>
+            <li>اعطای نمایندگی</li>
+            <li>درباره ما</li>
+            <li>تماس با ما</li>
+        </ul>
+    </div>
+    <div>
+        <ul className={styles.leftIcons}>
+            <li><Search/></li>
+            <li><Cart3/></li>
+            <li><Person/></li>
+        </ul>
+    </div>
+
+    </header>
+  )
+}
